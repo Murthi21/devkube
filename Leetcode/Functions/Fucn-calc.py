3f8917b92aeed355d58ab8417596c4067ee41676
Moorthi <murthi21@gmail.com>
2024-06-16 08:36:56 +0000

    added

---
+++ Leetcode/Functions/Fucn-calc.py
@@ -0,0 +1,21 @@
+a=int(input("Enter the number"))
+b=int(input("Enter the b number"))
+
+#print(int('geeks'))
+
+def add():
+    a1= a + b
+    print("Added by fun", a1)
+
+def sub():
+    s= a - b
+    print("sub by fun " ,s)
+
+def mul():
+    m= a * b
+    print("mul by fun",m)
+
+
+add()
+sub()
+mul()
